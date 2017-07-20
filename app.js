f23b408a77927c0cf9d995ce3c74266ab2074d8f
Damir Kotoric <damirkotoric@gmail.com>
2017-07-20 14:08:50 +0200

    removed old static site and blank node project is ready

---
+++ app.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session) // Helps store sessions in a database so we don't crash the server by storing them in server RAM
+const app = express()
+
+// mongodb connection
+mongoose.connect('mongodb://localhost:27017/minihero')
+const db = mongoose.connection
+// mongo error
+db.on('error', console.error.bind(console, 'connection error: '))
+
+// parse incoming requests
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// serve static files from /public
+app.use(express.static(__dirname + '/public'))
+
+// view engine setup
+app.set('view engine', 'pug')
+app.set('views', __dirname + '/views')
+
+// include routes
+const routes = require('./routes/index')
+app.use('/', routes)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('File not found.')
+  err.status = 404
+  next(err)
+})
+
+// error handler
+// define as the last app.use callback
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500)
+  res.render('error', {
+    message: err.message,
+    error: {}
+  })
+})
+
+// listen on port 3000
+app.listen(3000, function() {
+  console.log('Express app listening on port 3000')
+})
--- source/javascripts/all.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-  $(".hero__video").fitVids();
-});
