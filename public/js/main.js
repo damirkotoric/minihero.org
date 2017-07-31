2d0aa060e7a2ee6c7f043d9758d91f9b4ab359d2
Damir Kotoric <damirkotoric@gmail.com>
2017-08-01 01:32:37 +0200

    major static front-end components ready

--- public/js/main.js
+++ public/js/main.js
@@ -55,18 +55,20 @@ exports.init = function() {
 const Helper = require('../utilities/helper')
 
 exports.init = function() {
-  var map = document.querySelector('.map')
+  var map = document.getElementById('map__container')
   if (map) {
     // Create the map with no initial style specified.
     // It therefore has default styling.
-    map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(map, {
       center: {lat: 52.3628317, lng: 4.908374},
       zoom: 13,
       mapTypeControl: false,
       maxZoom: 15,
       streetViewControl: false,
       zoomControl: false,
-      backgroundColor: '#444444'
+      backgroundColor: '#444444',
+      scrollwheel: false,
+      disableDoubleClickZoom: true
     })
     map.setOptions({styles: styles['minihero']});
   }
@@ -75,94 +77,6 @@ exports.init = function() {
 var styles = {
   default: null,
   minihero:
-  // [
-  //   {
-  //     elementType: 'geometry',
-  //     stylers: [{color: '#f5f5f5'}]
-  //   },
-  //   {
-  //     elementType: 'labels.icon',
-  //     stylers: [{visibility: 'off'}]
-  //   },
-  //   {
-  //     elementType: 'labels.text.fill',
-  //     stylers: [{color: '#616161'}]
-  //   },
-  //   {
-  //     elementType: 'labels.text.stroke',
-  //     stylers: [{color: '#f5f5f5'}]
-  //   },
-  //   {
-  //     featureType: 'administrative.land_parcel',
-  //     elementType: 'labels.text.fill',
-  //     stylers: [{color: '#bdbdbd'}]
-  //   },
-  //   {
-  //     featureType: 'poi',
-  //     elementType: 'geometry',
-  //     stylers: [{color: '#eeeeee'}]
-  //   },
-  //   {
-  //     featureType: 'poi',
-  //     elementType: 'labels.text.fill',
-  //     stylers: [{color: '#757575'}]
-  //   },
-  //   {
-  //     featureType: 'poi.park',
-  //     elementType: 'geometry',
-  //     stylers: [{color: '#e5e5e5'}]
-  //   },
-  //   {
-  //     featureType: 'poi.park',
-  //     elementType: 'labels.text.fill',
-  //     stylers: [{color: '#9e9e9e'}]
-  //   },
-  //   {
-  //     featureType: 'road',
-  //     elementType: 'geometry',
-  //     stylers: [{color: '#ffffff'}]
-  //   },
-  //   {
-  //     featureType: 'road.arterial',
-  //     elementType: 'labels.text.fill',
-  //     stylers: [{color: '#757575'}]
-  //   },
-  //   {
-  //     featureType: 'road.highway',
-  //     elementType: 'geometry',
-  //     stylers: [{color: '#dadada'}]
-  //   },
-  //   {
-  //     featureType: 'road.highway',
-  //     elementType: 'labels.text.fill',
-  //     stylers: [{color: '#616161'}]
-  //   },
-  //   {
-  //     featureType: 'road.local',
-  //     elementType: 'labels.text.fill',
-  //     stylers: [{color: '#9e9e9e'}]
-  //   },
-  //   {
-  //     featureType: 'transit.line',
-  //     elementType: 'geometry',
-  //     stylers: [{color: '#e5e5e5'}]
-  //   },
-  //   {
-  //     featureType: 'transit.station',
-  //     elementType: 'geometry',
-  //     stylers: [{color: '#eeeeee'}]
-  //   },
-  //   {
-  //     featureType: 'water',
-  //     elementType: 'geometry',
-  //     stylers: [{color: '#c9c9c9'}]
-  //   },
-  //   {
-  //     featureType: 'water',
-  //     elementType: 'labels.text.fill',
-  //     stylers: [{color: '#9e9e9e'}]
-  //   }
-  // ]
   [
     {
         "featureType": "all",
